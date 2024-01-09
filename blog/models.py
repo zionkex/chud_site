f50a71c1d5de3b2b443cba61443fd654669e9247
zionkex <gicubruja@gmail.com>
2024-01-09 08:52:55 +0200

    add priority for menu options

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-# Create your models here
-class Menu (models.Model):
-    title = models.CharField(max_length =20)
-    icon = models.CharField(max_length = 50)
+class Menu(models.Model):
+    title = models.CharField(max_length=20)
+    icon = models.CharField(max_length=50)
+    priority = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['priority']
+        indexes = [
+            models.Index(fields=['priority']),
+        ]
