cb317c3f38925eee69d95246bcaffaf420b605c8
zionkex <gicubruja@gmail.com>
2024-01-09 09:02:11 +0200

    add title for a tag

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -1,33 +1,48 @@
-const body = document.querySelector('body'),
-      sidebar = body.querySelector('nav'),
-      toggle = body.querySelector(".toggle"),
-      searchBtn = body.querySelector(".search-box"),
-      modeSwitch = body.querySelector(".toggle-switch"),
-      modeText = body.querySelector(".mode-text");
+document.addEventListener('DOMContentLoaded', function() {
+    const body = document.querySelector('body');
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarLinks = document.querySelectorAll('.sidebar a');
 
+    body.addEventListener('click', function(event) {
+        if (!event.target.closest('.sidebar')) {
+            sidebar.classList.remove('close');
+        }
+    });
 
-toggle.addEventListener("click" , () =>{
-    sidebar.classList.toggle("close");
-})
+    sidebarLinks.forEach(link => {
+        link.addEventListener('mouseover', function() {
+            if (sidebar.classList.contains('close')) {
+                const title = this.getAttribute('data-temp-title');
+                if (title) {
+                    this.setAttribute('title', title);
+                    this.removeAttribute('data-temp-title');
+                }
+            } else {
+                const title = this.getAttribute('title');
+                if (title) {
+                    this.setAttribute('data-temp-title', title);
+                    this.removeAttribute('title');
+                }
+            }
+        });
+    });
 
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
+    const modeSwitch = document.querySelector('.toggle-switch');
+    const modeText = document.querySelector('.mode-text');
 
-modeSwitch.addEventListener("click" , () =>{
-    body.classList.toggle("dark");
+    modeSwitch.addEventListener('click', () => {
+        body.classList.toggle('dark');
+        if (body.classList.contains('dark')) {
+            modeText.innerText = 'Light mode';
+        } else {
+            modeText.innerText = 'Dark mode';
+        }
+    });
+
+    const toggle = document.querySelector('.toggle');
+    toggle.addEventListener('click', () => {
+        sidebar.classList.toggle('close');
+    });
+
     
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Light mode";
-    }else{
-        modeText.innerText = "Dark mode";
-        
-    }
 });
-
-
-$(document).ready(function() {
-    var numOfItems = $('.menu-links li').length;
-    $('.menu-links').css('--num-items', numOfItems);
-  });
-  
