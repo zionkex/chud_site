f505a01951f21e57739ac69f2e3a668d3d8d0a32
zionkex <gicubruja@gmail.com>
2024-01-13 14:02:21 +0200

    remove url  /menutry

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .models import Menu    
 # Create your views here.
-def first (request):
-    return render(request,'blog/base/base.html')
+
 def menu (request):
     options= Menu.objects.all()
     return render (request,'blog/menulist.html', {"options":options})
