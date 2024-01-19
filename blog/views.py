f74fa23ef2062eecfb8dd9f0f91312f0f0036343
zionkex <gicubruja@gmail.com>
2024-01-19 10:48:34 +0200

    adding post with photo

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from .models import Menu    
+from .models import Menu, PhotoPost
 # Create your views here.
 
 def menu (request):
     options= Menu.objects.all()
-    return render (request,'blog/menulist.html', {"options":options})
+    return render (request,'blog/base/base.html', {"options":options})
+
+def post_list(request):
+    posts = PhotoPost.published.all()
+    return render(request, 'blog/posts.html', {'posts': posts})
--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', views.menu, name ='first'),
+    path('posts/', views.post_list, name='post_list'),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
