01fe04ce005e246423292d2f019bb83851497502
zionkex <gicubruja@gmail.com>
2024-01-22 00:08:05 +0200

    adding setting for work with photo

--- core/urls.py
+++ core/urls.py
@@ -14,12 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', views.menu, name ='first'),
-    path ('posts/', views.post_list),
-    ]
+    path('', views.menu, name='first'),
+    path('posts/', views.post_list),
+    path('photo-posts/', views.photo_post_list, name='photo_post_list'),
+
+]
+
+# Serving static files during development
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
