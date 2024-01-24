f4324572ac014e58336deb41486e8c44da7d06ed
zionkex <gicubruja@gmail.com>
2024-01-24 23:49:36 +0200

    add form for multiple images upload

--- blog/forms.py
+++ blog/forms.py
@@ -1,15 +1,35 @@
 from django import forms
-from .models import PhotoPost, Fileupload
+from .models import Fileupload
 
 
-class PhotoPostForm(forms.ModelForm):
-    class Meta:
-        model = PhotoPost
-        fields = ['title', 'body', 'images']
-        widgets = {
-            'images': forms.CheckboxSelectMultiple(),
-        }
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
 
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+class PostForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    body = forms.CharField(widget=forms.Textarea)
+    main_image = forms.ImageField(required=False)
+    images = MultipleFileField()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        main_image = cleaned_data.get('main_image')
+        images = cleaned_data.get('images')
+
+        return cleaned_data
 
 class FileuploadForm(forms.ModelForm):
     class Meta:
