adf7a1cb67c8b00b26c0ca89e726e2fefb9b35e2
zionkex <gicubruja@gmail.com>
2024-01-25 00:07:11 +0200

    updaate

--- core/urls.py
+++ core/urls.py
@@ -24,17 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.menu, name='first'),
     path('posts/', views.post_list),
-<<<<<<< HEAD
     path('create_post/', views.PostFormView.as_view(), name='create_post'),
     path('home/', views.home ),
-    path('files/', views.upload_file, name='upload_file')
-
-=======
-    path('photo-posts/', views.photo_post_list, name='photo_post_list'),
     path('file/', views.upload_file, name='upload_file'),
-    path ('files/<slug:slug>/', views.open_pdf, name='view_pdf'),
     path('upload/', views.upload_file),
->>>>>>> 5d7547160f1e42845dc0922713855832685d4ce0
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
